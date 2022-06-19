307d941201f3240dd9896a85569bc1377fba216b
sylvia-H <sylvie11gaia@gmail.com>
2022-06-19 11:54:52 +0800

    revise redirect URL

--- connection/passport.js
+++ connection/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      callbackURL: `${process.env.BASE_URL}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, cb) => {
       // 驗證使用者是否已存在
